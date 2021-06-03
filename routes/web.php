49eb22c583085d78ecbf8186fddf243664d6cdd7
VanNguyen-Kumo <thivan.nguyen@staff.gumiviet.com>
2021-06-03 09:53:55 +0700

    no message

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,3 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    Cart::add(['id' => '293ad', 'name' => 'Product 1', 'qty' => 1, 'price' => 9.99, 'weight' => 550, 'options' => ['size' => 'large']]);
-    return Cart::get('id');
-});
