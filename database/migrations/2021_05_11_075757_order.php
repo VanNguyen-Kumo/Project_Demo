761c363add5611f502531a40b3076fed50d2fe25
VanNguyen-Kumo <thivan.nguyen@staff.gumiviet.com>
2021-06-09 16:03:13 +0700

    no message

--- database/migrations/2021_05_11_075757_order.php
+++ database/migrations/2021_05_11_075757_order.php
@@ -16,11 +16,11 @@ class Order extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->uuid('id');
             $table->integer('total_price');
-            $table->integer('total_quantity');
             $table->string('delivery_address');
             $table->string('delivery_date');
             $table->integer('phone');
-            $table->string('status');
+            $table->enum('status', OrderStatusType::getValues())
+                ->default(OrderStatusType::WaitForConfirmation)->change();
             $table->uuid('user_id');
             $table->primary('id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete ('cascade')->onUpdate('cascade');
