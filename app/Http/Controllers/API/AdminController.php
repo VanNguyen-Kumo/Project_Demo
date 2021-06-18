0175b68ba0a48deb018699995869262487ac2d1b
VanNguyen-Kumo <thivan.nguyen@staff.gumiviet.com>
2021-06-18 16:00:25 +0700

    no message

--- app/Http/Controllers/API/AdminController.php
+++ app/Http/Controllers/API/AdminController.php
@@ -12,7 +12,7 @@ class AdminController extends Controller
 {
     public function index()
     {
-        $admin = Admin::query()->where('username', 'LIKE', '%' . request('keyword') . '%')->orderBy('updated_at','DESC')->paginate(config('constants.paginate'));;
+        $admin = Admin::query()->where('username', 'LIKE', '%' . request('keyword') . '%')->orderBy('updated_at','DESC');
         return  response()->json(['data'=>$admin,'message'=>'Login success']);
     }
 
